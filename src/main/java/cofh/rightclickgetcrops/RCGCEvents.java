ed4a9c0781427de3e5cdfcc8439037d99f2d05b6
King Lemming <kinglemming@gmail.com>
2022-05-04 21:32:19 -0700

    Port to 1.18.2

--- src/main/java/cofh/rightclickgetcrops/RCGCEvents.java
+++ src/main/java/cofh/rightclickgetcrops/RCGCEvents.java
@@ -1,19 +1,19 @@
 package cofh.rightclickgetcrops;
 
 import cofh.lib.block.IHarvestable;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.CropsBlock;
-import net.minecraft.block.NetherWartBlock;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.InventoryHelper;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Direction;
-import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.NetherWartBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -45,8 +45,8 @@ public class RCGCEvents {
     @SubscribeEvent (priority = EventPriority.HIGHEST)
     public static void handleRightClickBlockEvent(PlayerInteractEvent.RightClickBlock event) {
 
-        World world = event.getWorld();
-        if (event.getHand() == Hand.OFF_HAND) {
+        Level world = event.getWorld();
+        if (event.getHand() == InteractionHand.MAIN_HAND) {
             return;
         }
         BlockPos pos = event.getPos();
@@ -57,7 +57,7 @@ public class RCGCEvents {
         if (RCGCConfig.allowList.get() != RCGCConfig.cropList.get().contains(block.getRegistryName().toString())) {
             return;
         }
-        PlayerEntity player = event.getPlayer();
+        Player player = event.getPlayer();
         boolean replant = RCGCConfig.replant.get();
 
         // IHarvestables are smart! They handle their own replanting.
@@ -65,11 +65,11 @@ public class RCGCEvents {
             IHarvestable harvestable = (IHarvestable) block;
             if (harvestable.canHarvest(state)) {
                 harvestable.harvest(world, pos, state, player, replant);
-                player.swing(Hand.MAIN_HAND);
+                player.swing(InteractionHand.MAIN_HAND);
                 event.setCanceled(true);
             }
-        } else if (block instanceof CropsBlock) {
-            CropsBlock crop = (CropsBlock) block;
+        } else if (block instanceof CropBlock) {
+            CropBlock crop = (CropBlock) block;
             boolean seedDrop = false;
 
             BlockPos below = pos.below();
@@ -80,7 +80,7 @@ public class RCGCEvents {
 
             if (crop.isMaxAge(state)) {
                 if (!world.isClientSide) {
-                    List<ItemStack> drops = Block.getDrops(state, (ServerWorld) world, pos, null, player, player.getMainHandItem());
+                    List<ItemStack> drops = Block.getDrops(state, (ServerLevel) world, pos, null, player, player.getMainHandItem());
                     Item seedItem = crop.getCloneItemStack(world, pos, state).getItem();
                     for (ItemStack drop : drops) {
                         if (replant && !seedDrop) {
@@ -90,7 +90,7 @@ public class RCGCEvents {
                             }
                         }
                         if (!drop.isEmpty()) {
-                            InventoryHelper.dropItemStack(world, pos.getX() + .5, pos.getY() + .5, pos.getZ() + .5, drop);
+                            Containers.dropItemStack(world, pos.getX() + .5, pos.getY() + .5, pos.getZ() + .5, drop);
                         }
                     }
                     world.destroyBlock(pos, false, player);
@@ -98,7 +98,7 @@ public class RCGCEvents {
                         world.setBlock(pos, crop.getStateForAge(0), 3);
                     }
                 }
-                player.swing(Hand.MAIN_HAND);
+                player.swing(InteractionHand.MAIN_HAND);
                 event.setCanceled(true);
             }
         } else if (block instanceof NetherWartBlock) {
@@ -113,7 +113,7 @@ public class RCGCEvents {
 
             if (state.getValue(NetherWartBlock.AGE) >= 3) {
                 if (!world.isClientSide) {
-                    List<ItemStack> drops = Block.getDrops(state, (ServerWorld) world, pos, null, player, player.getMainHandItem());
+                    List<ItemStack> drops = Block.getDrops(state, (ServerLevel) world, pos, null, player, player.getMainHandItem());
                     Item seedItem = crop.getCloneItemStack(world, pos, state).getItem();
                     for (ItemStack drop : drops) {
                         if (replant && !seedDrop) {
@@ -123,7 +123,7 @@ public class RCGCEvents {
                             }
                         }
                         if (!drop.isEmpty()) {
-                            InventoryHelper.dropItemStack(world, pos.getX() + .5, pos.getY() + .5, pos.getZ() + .5, drop);
+                            Containers.dropItemStack(world, pos.getX() + .5, pos.getY() + .5, pos.getZ() + .5, drop);
                         }
                     }
                     world.destroyBlock(pos, false, player);
@@ -131,7 +131,7 @@ public class RCGCEvents {
                         world.setBlock(pos, crop.defaultBlockState(), 3);
                     }
                 }
-                player.swing(Hand.MAIN_HAND);
+                player.swing(InteractionHand.MAIN_HAND);
                 event.setCanceled(true);
             }
         }
